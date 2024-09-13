9a3589468c0878a8ea79f08dbd0a9fd99207b94a
Dima Somik <ghubdmytrosomik@gmail.com>
2024-09-13 02:15:12 +0200

    Completed Project

---
+++ src/02-form.js
@@ -0,0 +1,44 @@
+const form = document.querySelector(".feedback-form");
+const emailInput = document.querySelector(".form-email")
+const textArea = document.querySelector(".form-area")
+const localStorageKey = "feedback-form-state";
+const STORAGE = {
+    email: "",
+    message: "",
+}
+
+const getLocalStorage = JSON.parse(localStorage.getItem(localStorageKey)) ?? "";
+
+if (localStorage.getItem(localStorageKey) == null) {
+    emailInput.value = "";
+    textArea.value = "";
+} else {
+    emailInput.value = getLocalStorage.email;
+    textArea.value = getLocalStorage.message;
+};
+
+form.addEventListener("input", (event) => {
+    switch (event.target.className) {
+        case "form-email":
+            STORAGE.email = event.target.value;
+            localStorage.setItem(localStorageKey, JSON.stringify(STORAGE).trim());       
+            break;
+        case "form-area":
+            STORAGE.message = event.target.value;
+            localStorage.setItem(localStorageKey, JSON.stringify(STORAGE).trim());       
+            break;
+        default:
+            break;
+    }
+});
+
+form.addEventListener("submit", (event) => {
+    if (emailInput.value == "" || textArea.value == "") {
+        alert("You can't leave blank fields!"); 
+    } else {
+        event.preventDefault();
+        console.log(STORAGE);
+        localStorage.removeItem(localStorageKey);
+        form.reset();
+    };
+});
